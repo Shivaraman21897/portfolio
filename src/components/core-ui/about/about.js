daab0f321e04d49f464ad019b465b3a22d9e5831
sivaraman <sivaraman.m@proglint.com>
2023-05-17 10:46:37 +0530

    added data

--- src/components/core-ui/about/about.js
+++ src/components/core-ui/about/about.js
@@ -21,6 +21,10 @@ function About() {
                     <div className="about-description">
                         <h2 style={{ color: theme.primary }}>{aboutData.title}</h2>
                         <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description2}</p>
+                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description3}</p>
+                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description4}</p>
+                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description5}</p>
+                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description6}</p>
                     </div>
                     <div className="about-animation">
                         <AnimationLottie animationPath={codings} />
--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,11 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is SIVARAMAN. I'm a web designer and developer based in Chennai, Tamil Nadu.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.",
+    description1: "Hi, I'm Sivaraman, a web designer and developer based in Chennai, Tamil Nadu. I am a professional and passionate programmer with a strong focus on web development.",
+    description2: "In my daily life, I embrace the role of a quick learner with a self-learning attitude. I am constantly motivated to learn and explore new technologies, keeping up with the ever-evolving landscape of software engineering. I find great joy in diving deep into different stacks and frameworks to expand my skill set and stay ahead of the curve ",
+    description3:"Currently, my stack revolves around Node.js, React, Redux, Jest, Bootstrap, Material UI, MongoDB, MySQL, and more. These technologies empower me to build robust and scalable web applications. I am proficient in utilizing Node.js for server-side development, while React and Redux allow me to create dynamic and interactive user interfaces",
+    description4:"As a problem solver at heart, I enjoy tackling challenges and finding elegant solutions. I approach problems with a logical and analytical mindset, breaking them down into manageable steps. This approach helps me create efficient algorithms and design elegant solutions that deliver tangible results",
+    description5:"In addition to my technical expertise, I value effective collaboration and communication. I have experience working in teams and understand the importance of clear communication to ensure smooth project execution. I actively contribute to a positive and productive team environment, bringing my skills and enthusiasm to deliver high-quality solutions.",
+    description6:"If you have any exciting projects or opportunities, I would be delighted to connect and discuss how my skills and passion for programming can contribute to their success. Let's collaborate and build something amazing together!"
 }
+
+``
